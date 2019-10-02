b8787c086bb76a2482faa513e58db66e8f8f5f68
StefMach <stefanmachura@gmail.com>
2019-10-02 12:40:27 +0200

    added database

--- logger/routes.py
+++ logger/routes.py
@@ -1,7 +1,9 @@
+from flask import render_template
 from logger import app
+
 
 @app.route('/')
 @app.route('/index')
-
 def index():
-    return "Hello world!"
+    user = {'username': 'Miguel'}
+    return render_template('index.html', user=user)

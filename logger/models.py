bd9affb025c38d80dc407a3957227b1c09256d9b
StefMach <stefanmachura@gmail.com>
2019-10-02 12:47:32 +0200

    added temperature to db

--- logger/models.py
+++ logger/models.py
@@ -9,3 +9,13 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+
+class Temperature(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    location = db.Column(db.String(128))
+    tmp = db.Column(db.String(10))
+    datetime = db.Column(db.String(128))
+
+    def __repr__(self):
+        return 'location: {} time: {} temperature: {}'.format(self.location, self.time, self.tmp)

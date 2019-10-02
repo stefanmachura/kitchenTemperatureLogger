0bfc47dfcfc38004aef746978b6447b7b9f5a837
StefMach <stefanmachura@gmail.com>
2019-10-02 13:47:11 +0200

    showing temp log

--- logger/routes.py
+++ logger/routes.py
@@ -1,9 +1,20 @@
 from flask import render_template
-from logger import app
+from logger import app, db
+from logger.models import Temperature
+import random
 
 
 @app.route('/')
 @app.route('/index')
 def index():
+    temp = str(random.randrange(1000) / 100)
+    x = Temperature(location='kitchen', tmp=temp)
+    db.session.add(x)
+    db.session.commit()
+
+
+
     user = {'username': 'Miguel'}
-    return render_template('index.html', user=user)
+    t = Temperature.query.all()
+    data = {'temperatures': t}
+    return render_template('index.html', user=user, data=data)

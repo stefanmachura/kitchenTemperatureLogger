25cdaa56931708fa6fdea7635153940caf91bfc0
stfn1337 <stefanmachura@gmail.com>
2019-11-03 16:06:14 +0100

    added plots for different timespans

---
+++ logger/static/images.js
@@ -0,0 +1,6 @@
+function change(url) {
+    console.log(url)
+    image = document.getElementById("plot");
+    image.src = url;
+    console.log("I am changing");
+}
--- plot.py
+++ plot.py
@@ -9,17 +9,14 @@ import sys
 import timeit
 
 
-def generate_graph_from_db():
+def generate_graph_from_db(days):
     conn = sqlite3.connect('app.db')
     c = conn.cursor()
 
     how_many = 60 if len(sys.argv) == 1 else sys.argv[1]
     data = []
     time = []
-    for row in c.execute("""SELECT *
-                            FROM temperature
-                            ORDER BY timestamp
-                            DESC LIMIT ?""", (how_many, )):
+    for row in c.execute("SELECT * FROM temperature WHERE timestamp > DATETIME('now', ?) ORDER BY timestamp", (days, )):
         data.append(float(row[2]))
         tm = datetime.strptime(row[3], "%Y-%m-%d %H:%M:%S.%f")
         time.append(tm)
@@ -27,16 +24,17 @@ def generate_graph_from_db():
     fig, ax = plt.subplots()
     ax.plot(time, data)
 
-    myFmt = DateFormatter("%H:%M")
-    ax.xaxis.set_major_formatter(myFmt)
-
-    fig.autofmt_xdate()
+    plt.xticks([time[0], time[-1]], visible=True, rotation="horizontal")
 
     basedir = os.path.abspath(os.path.dirname(__file__))
-    fname = os.path.join(basedir, 'logger', 'static', 'plot.png')
-    plt.savefig(fname)
+
+    fname = days.replace('-', '').replace(' ', '') + '.png'
+    plot_file = os.path.join(basedir, 'logger', 'static', fname)
+    plt.savefig(plot_file)
 
     conn.close()
 
 
-print(timeit.timeit(generate_graph_from_db, number=1))
+generate_graph_from_db('-1 day')
+generate_graph_from_db('-3 days')
+generate_graph_from_db('-10 days')
